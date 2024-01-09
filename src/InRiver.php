827bc4b3554bf3e305768ab9462ffcbb0a41cf59
Samuel Loft <sam@devloft.co.uk>
2024-01-09 13:59:47 +0000

    Added initial properties and construct

--- src/InRiver.php
+++ src/InRiver.php
@@ -6,5 +6,15 @@ namespace Scolmore\InRiver;
 
 class InRiver
 {
-    // Build wonderful things
+    public string $version = '1.0.0';
+    public ?string $url;
+    public ?string $apiKey;
+
+    public function __construct()
+    {
+        $url = config('inriver.inriver-url');
+
+        $this->url = "{$url}/api/v{$this->version}/";
+        $this->apiKey = config('inriver.inriver-api-key');
+    }
 }
