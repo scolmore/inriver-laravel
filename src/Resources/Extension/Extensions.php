19856bf91123b29926eacff06ea448a33cea398b
Samuel Loft <sam@devloft.co.uk>
2024-01-11 14:47:36 +0000

    WIP

---
+++ src/Resources/Extension/Extensions.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Scolmore\InRiver\Resources\Extension;
+
+use Scolmore\InRiver\Resources\AbstractResource;
+
+class Extensions extends AbstractResource
+{
+    protected string $endpoint = 'extensions';
+}
