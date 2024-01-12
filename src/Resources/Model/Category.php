c002dcd46c7633de15db16d11c639d2633a584ed
Samuel Loft <sam@devloft.co.uk>
2024-01-12 10:02:17 +0000

    WIP

--- src/Resources/Model/Category.php
+++ src/Resources/Model/Category.php
@@ -6,5 +6,91 @@ namespace Scolmore\InRiver\Resources\Model;
 
 trait Category
 {
+    /**
+     * Get all categories.
+     *
+     * @return array
+     *
+     * @see https://apieuw.productmarketingcloud.com/swagger/index.html#/Model/GetAllCategories
+     */
+    public function getAllCategories(): array
+    {
+        return $this->inRiver()->request(
+            method: 'GET',
+            endpoint: $this->endpoint('/category')
+        );
+    }
 
+    /**
+     * Add a category.
+     *
+     * @param  string  $id
+     * @param  string  $name
+     * @return array
+     *
+     * @see https://apieuw.productmarketingcloud.com/swagger/index.html#/Model/AddCategory
+     */
+    public function addCategory(string $id, string $name): array
+    {
+        return $this->inRiver()->request(
+            method: 'POST',
+            endpoint: $this->endpoint('/category'),
+            data: [
+                'id' => $id,
+                'name' => $name,
+            ]
+        );
+    }
+
+    /**
+     * Get a category.
+     *
+     * @param  string  $categoryId
+     * @return array
+     *
+     * @see https://apieuw.productmarketingcloud.com/swagger/index.html#/Model/GetCategory
+     */
+    public function getCategory(string $categoryId): array
+    {
+        return $this->inRiver()->request(
+            method: 'GET',
+            endpoint: $this->endpoint("/category/{$categoryId}")
+        );
+    }
+
+    /**
+     * Update a category.
+     *
+     * @param  string  $categoryId
+     * @param  string  $name
+     * @return array
+     *
+     * @see https://apieuw.productmarketingcloud.com/swagger/index.html#/Model/UpdateCategory
+     */
+    public function updateCategory(string $categoryId, string $name): array
+    {
+        return $this->inRiver()->request(
+            method: 'PUT',
+            endpoint: $this->endpoint("/category/{$categoryId}"),
+            data: [
+                'name' => $name,
+            ]
+        );
+    }
+
+    /**
+     * Delete a category.
+     *
+     * @param  string  $categoryId
+     * @return array
+     *
+     * @see https://apieuw.productmarketingcloud.com/swagger/index.html#/Model/DeleteCategory
+     */
+    public function deleteCategory(string $categoryId): array
+    {
+        return $this->inRiver()->request(
+            method: 'DELETE',
+            endpoint: $this->endpoint("/category/{$categoryId}")
+        );
+    }
 }
